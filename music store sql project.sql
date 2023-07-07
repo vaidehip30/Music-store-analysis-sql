7920481072c7911e0e6f0c7bdc5e171efc554927
vaidehip30 <134762504+vaidehip30@users.noreply.github.com>
2023-07-07 21:24:42 +0530

    music store sql project.sql

--- music store sql project.sql
+++ music store sql project.sql
@@ -98,25 +98,6 @@ join best_selling_artist bsa on bsa.artist_id=alb.artist_id
 group by 1,2,3,4
 order by 5 desc
 
-/* Q9: We want to find out the most popular music Genre for each country. We determine the 
-most popular genre as the genre with the highest amount of purchases. Write a query 
-that returns each country along with the top Genre. For countries where the maximum 
-number of purchases is shared return all Genres.*/
 
-with most_popular_genre as(
-	select count(invoice_line.quantity)as purchases,customer.country,genre.name,
-	genre.genre_id,
-	row_number() over(partition by customer.country order by count(invoice_line.quantity)desc)
-	as rownumber
-from invoice_line
-	join invoice on invoice.invoice_id=invoice_line.invoice_id
-	join customer on customer.customer_id=invoice.customer_id
-	join track on track.track_id=invoice_line.track_id
-	join genre on genre.genre_id= track.genre_id
-	group by 2,3,4
-	order by 2 asc,1 desc
-
-)
-select*from most_popular_genre where rownumber<=1
 
 
