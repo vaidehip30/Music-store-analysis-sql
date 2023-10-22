6837cde06d4cf6116eca053a95671d68b793c785
vaidehip30 <134762504+vaidehip30@users.noreply.github.com>
2023-10-22 17:40:43 +0530

    music store sql project.sql

--- music store sql project.sql
+++ music store sql project.sql
@@ -48,26 +48,3 @@ join genre on genre.genre_id=track.genre_id
 where genre.name like 'Rock'
 order by email
 
-/* Q6:Let's invite the artists who have written the most rock music in our dataset. 
-Write a query that returns the Artist name and total track count of the
-top 10 rock bands.*/
-
-select artist.artist_id,artist.name,count(artist.artist_id) as total_songs
-from track
-join album on album.album_id=track.album_id
-join artist on artist.artist_id=album.artist_id
-join genre on genre.genre_id=track.genre_id
-group by artist.artist_id
-order by total_songs desc
-limit 10
-
-/* Q7:Return all the track names that have a song length longer than the 
-average song length. Return the Name and Milliseconds for each track. Order by 
-the song length with the longest songs listed first.*/
-
-
-select name,milliseconds from track
-where milliseconds>(select avg(milliseconds) as average_tracklength from track)
-order by milliseconds desc
-
-
